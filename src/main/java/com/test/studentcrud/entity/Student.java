3614ae2aed8f4c1945bfd33aeb2f10701b6572f6
MahediBuilds <zainmahedi.cs23@bmsce.ac.in>
2025-07-24 14:24:26 +0400

    Code Cleanup

--- src/main/java/com/test/studentcrud/entity/Student.java
+++ src/main/java/com/test/studentcrud/entity/Student.java
@@ -1,6 +1,9 @@
 package com.test.studentcrud.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
@@ -21,16 +24,36 @@ public class Student {
     @NotBlank(message = "Course is mandatory")
     private String course;
 
-    // Getters and Setters
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public Integer getId() {
+        return id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public String getCourse() { return course; }
-    public void setCourse(String course) { this.course = course; }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+         }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
